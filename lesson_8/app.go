f2702801c8416d643d736d2c1d00f62be4c64cea
Alexander Sharov <dronline872@gmail.com>
2021-10-31 16:50:41 +0300

    урок 9

--- lesson_8/app.go
+++ lesson_8/app.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -12,6 +14,13 @@ var (
 	jaeger_url = flag.String("jaeger-url", "http://jaeger:16686", "Ссылка на jaeger")
 	sentry_url = flag.String("sentry-url", "http://sentry:9000", "Ссылка на sentry")
 )
+
+type App struct {
+	Port        int    `json:"port"`
+	KafkaBroker string `json:"kafka_broker"`
+	SomeAppId   string `json:"some_app_id"`
+	SomeAppKey  string `json:"some_app_key"`
+}
 
 func main() {
 	os.Setenv("kafka_broker", "kafka:9092")
@@ -26,4 +35,26 @@ func main() {
 	fmt.Println("kafka_broker: ", os.Getenv("kafka_broker"))
 	fmt.Println("some_app_id: ", os.Getenv("some_app_id"))
 	fmt.Println("some_app_key: ", os.Getenv("some_app_key"))
+
+	//Урок 9
+	file, err := os.Open("conf.json")
+	if err != nil {
+		log.Fatalf("Не могу открыть файл: %v", err)
+	}
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Printf("Не могу закрыть файл: %v", err)
+		}
+	}()
+
+	app := App{}
+
+	err = json.NewDecoder(file).Decode(&app)
+	if err != nil {
+		log.Printf("Не могу декодировать json-файл в структуру: %v", err)
+	}
+
+	fmt.Println(app.Port)
 }
